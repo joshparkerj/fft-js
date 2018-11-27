7743a68053f326297b614bf88ad6ea617c95ebf7
joshparkerj <superfakek@gmail.com>
2018-11-27 15:31:42 -0700

    get the compression working!

---
+++ ave-comp.py
@@ -0,0 +1,27 @@
+'''
+Thursday's attempt to compress wavefile data using fft.
+
+First argument: name of wave file.
+Second argument: percentage of data you want thrown out.
+
+Usage example:
+python ave-comp.py sound.wav 99
+'''
+import numpy as np
+from scipy.io import wavfile
+import sys
+import pickle
+
+rate, data = wavfile.read(sys.argv[1])
+datalength = len(data)
+
+dft = np.fft.fft(data).astype(np.complex64)
+dft_cutoff = np.percentile(np.abs(dft), float(sys.argv[2]))
+dft_highlights = []
+
+for i,x in np.ndenumerate(dft):
+ if abs(x) >= dft_cutoff:
+  dft_highlights.append((i[0],x))
+output = {'rate': rate, 'data': dft_highlights, 'length': datalength}
+with open(sys.argv[1] + '.fft','wb') as file:
+ pickle.dump(output,file)
---
+++ ave-decomp.py
@@ -0,0 +1,22 @@
+'''
+Thursday's attempt to decompress wavefile data using fft.
+
+First argument: name of compressed file.
+
+Usage example:
+python ave-decomp.py sound.wav.fft
+'''
+import numpy as np
+from scipy.io import wavfile
+import sys
+import pickle
+
+with open(sys.argv[1],'rb') as file:
+ x = pickle.load(file)
+
+dft = [0+0j for i in range(x['length'])]
+for datum in x['data']:
+ dft[datum[0]] = datum[1]
+
+newdata = np.fft.ifft(dft).astype(np.int16)
+wavfile.write(sys.argv[1] + '.wav', x['rate'], newdata)
